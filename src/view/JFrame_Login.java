ee05b38b09ea8e66636232d25362a89bbbb76f0e
huylequang <huylequang@10.2.8.85>
2023-05-09 19:17:08 +0700

    Cập nhật tính năng Login

--- src/view/JFrame_Login.java
+++ src/view/JFrame_Login.java
@@ -120,7 +120,6 @@ public class JFrame_Login extends javax.swing.JFrame {
         // TODO add your handling code here:
         this.txt_TaiKhoan.setText("");
         this.passwordField.setText("");
-        System.out.println("");
     }//GEN-LAST:event_btn_HuyActionPerformed
 
     private void btn_DangNhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_DangNhapActionPerformed
